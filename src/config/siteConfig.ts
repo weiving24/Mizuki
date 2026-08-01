91b86cad1e629f6e7007e502bbd37487e5a48145
weiving <lkx9873@163.com>
2026-08-01 12:56:32 +0800

    new update2

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "Starsh日常",
 	subtitle: "One demo website",
-	siteURL: "https://leeking.cn/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://leekingx.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-07-31", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
