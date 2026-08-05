72125a8c9b359087d3168d4388fbc427a962f871
weiving <lkx9873@163.com>
2026-08-05 20:42:45 +0800

    new update9

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -109,7 +109,7 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
-				"/assets/desktop-banner/sjchdesk.png",
+				"/assets/desktop-banner/sjchdesk.webp",
 //				"/assets/desktop-banner/2.webp",
 //				"/assets/desktop-banner/3.webp",
 //				"/assets/desktop-banner/4.webp",
