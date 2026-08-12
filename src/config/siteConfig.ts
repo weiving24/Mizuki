50f2bb98bb9011b56ec2b54d5137266a78a4d706
weiving <lkx9873@163.com>
2026-08-12 22:32:05 +0800

    new update11

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -38,7 +38,7 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
 		icon: "assets/home/home.webp",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.webp",
+		logo: "assets/home/home.webp",
 	},
 
 	// 页面自动缩放配置
